f7e4fc2713d9b830eddf21b8982b863f208b1601
Ori Pekelman <ori@pekelman.com>
2019-02-18 11:09:24 +0100

    let us insist

--- platform_sh_update_nodejs.sh
+++ platform_sh_update_nodejs.sh
@@ -2,5 +2,6 @@ unset NPM_CONFIG_PREFIX
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | dash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-nvm current
 nvm install node
+nvm alias default node
+ln -s /app/.nvm/versions/node/v11.10.0/bin/node /app/.nvm/versions/node/v11.10.0/bin/nodejs
